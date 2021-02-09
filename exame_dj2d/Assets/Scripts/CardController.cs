2c912aafecff40e82c70cc82e8362ef4913f5188
Bruno Gomes <bruno.gomes.771@outlook.com>
2021-02-09 10:34:59 +0000

    update1

--- exame_dj2d/Assets/Scripts/CardController.cs
+++ exame_dj2d/Assets/Scripts/CardController.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class CardController : MonoBehaviour
 {
     public Card cardInfo;
+
+    public int manaCost;
+    public int attack;
+    public int health;
 
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI descriptionText;
@@ -20,20 +25,25 @@ public class CardController : MonoBehaviour
     {
         if (cardInfo != null)
         {
-            UpdateInfo();
+            GetCardInfo();
         }
         else
         {
             Destroy(this.gameObject);
         }
+
+        TakeDamage(2);
     }
-    public void UpdateInfo()
+    public void GetCardInfo()
     {
         nameText.text = cardInfo.name;
         descriptionText.text = cardInfo.description;
         manaText.text = cardInfo.manaCost.ToString();
+        manaCost = cardInfo.manaCost;
         attackText.text = cardInfo.attack.ToString();
+        attack = cardInfo.attack;
         healthText.text = cardInfo.health.ToString();
+        health = cardInfo.health;
         artwork.sprite = cardInfo.cardSprite;
 
         if (cardInfo.name.Length > 11)
@@ -44,5 +54,24 @@ public class CardController : MonoBehaviour
         {
             nameText.fontSize = 35f;
         }
+    }
+
+    public void UpdateCardInfo()
+    {
+        manaText.text = manaCost.ToString();
+        attackText.text = attack.ToString();
+        healthText.text = health.ToString();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        UpdateCardInfo();
+        if (health <= 0) { Die(); }
+    }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
     }
 }
---
+++ exame_dj2d/Assets/Scripts/ScriptableObjects Base/Deck.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Deck", menuName = "Deck")]
+public class Deck : ScriptableObject
+{
+    public new string name;
+    public Card[] cards;
+}
