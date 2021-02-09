5249b14695247e2fde6b2c7568caac1dd0012ce3
Bruno Gomes <bruno.gomes.771@outlook.com>
2021-02-09 09:51:06 +0000

    Initial Commit

---
+++ exame_dj2d/Assets/Scripts/CardController.cs
@@ -0,0 +1,48 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardController : MonoBehaviour
+{
+    public Card cardInfo;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI manaText;
+    public TextMeshProUGUI attackText;
+    public TextMeshProUGUI healthText;
+    public Image artwork;
+
+    private void Start()
+    {
+        if (cardInfo != null)
+        {
+            UpdateInfo();
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public void UpdateInfo()
+    {
+        nameText.text = cardInfo.name;
+        descriptionText.text = cardInfo.description;
+        manaText.text = cardInfo.manaCost.ToString();
+        attackText.text = cardInfo.attack.ToString();
+        healthText.text = cardInfo.health.ToString();
+        artwork.sprite = cardInfo.cardSprite;
+
+        if (cardInfo.name.Length > 11)
+        {
+            nameText.fontSize = 28f;
+        }
+        else
+        {
+            nameText.fontSize = 35f;
+        }
+    }
+}
---
+++ exame_dj2d/Assets/Scripts/ScriptableObjects Base/Card.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public new string name;
+    [TextArea]
+    public string description;
+    [Range(0, 10)]
+    public int manaCost;
+    public int health;
+    public int attack;
+
+    public Sprite cardSprite;
+}
